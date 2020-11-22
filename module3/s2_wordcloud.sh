4a2d0d3bbdc6706300097b64dedc39cacc4de785
Quentin <aubertquentin@gmail.com>
2020-11-22 16:45:12 +0100

    Mise à jour 22.11.2020

--- module3/s2_wordcloud.sh
+++ module3/s2_wordcloud.sh
@@ -5,8 +5,7 @@
 
 YEAR=$1
 cat data/txt/*_${YEAR}_*.txt > module3/${YEAR}.txt
-wordcloud_cli  --text module3/${YEAR}_keywords.txt 
 python3 module3/filtering.py 
-
+wordcloud_cli  --text module3/${YEAR}_keywords.txt 
 wordcloud_cli  --imagefile module3/${YEAR}.png --width 2000 --height 1000
 display module3/${YEAR}.png
